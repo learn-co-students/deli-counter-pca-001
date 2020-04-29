0d63ff23e2a3edd127c708d63cbbbe1c3a87a8f2
Pat McClernan <patmccler@gmail.com>
2020-04-29 16:59:40 -0400

    got initial functions working

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+
+def line deli
+  msg = "The line is currently:"
+  if deli.empty?
+     msg = "The line is currently empty."
+  else
+    deli.each_with_index {|person, i| msg << " #{i+1}. #{person}" }
+  end
+
+  puts msg
+end
+
+def take_a_number deli, person
+  deli << person
+  puts "Welcome, #{person}. You are number #{deli.size} in line."
+
+end
+
+def now_serving deli
+  if deli.size < 1
+    puts "There is nobody waiting to be served!"
+  else
+    serving = deli.shift
+      puts "Currently serving #{serving}."
+  end
+end
