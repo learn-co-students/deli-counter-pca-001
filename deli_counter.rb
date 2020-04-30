f1b5256d4c503008723ca642490a3666b8505555
Liam Tinney <ltinney@powerhrg.com>
2020-04-30 09:59:22 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    spot_in_queue = [];
+    if (katz_deli).empty?
+        puts "The line is currently empty."
+    else
+        number = 1
+        katz_deli.each do |customer|
+          spot_in_queue << "#{number}. #{customer}"
+          number += 1
+        end
+        puts "The line is currently: #{spot_in_queue.join(" ")}"
+    end
+end
+
+
+def take_a_number(katz_deli, customer)
+    katz_deli << customer
+        puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.first}."
+        katz_deli.shift 
+    end
+end
+
+
+
+
+
